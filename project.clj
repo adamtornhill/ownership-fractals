6bdd9ff230d5348344040a97830958611e573580
Adam Tornhill <adam@adamtornhill.com>
2015-05-15 11:25:35 +0200

    A brief project description

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject ownership-fractals "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Visualized knowledge ownership as fractal figures"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
