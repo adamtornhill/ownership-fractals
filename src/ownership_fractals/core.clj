9329c377f1a03298dd1cb704bea5122bf4706cfb
Adam Tornhill <adam@adamtornhill.com>
2015-05-17 09:44:29 +0200

    Prepare to support multiple entities

--- src/ownership_fractals/core.clj
+++ src/ownership_fractals/core.clj
@@ -4,6 +4,24 @@
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
+;;; Algorithm:
+;;; =========
+;;; We get CSV ownership data that looks like this:
+;;;    Entity, Owner, Parts Owned, Total Parts
+;;;    a.c,    adam,     50,          122
+;;;    a.c,     ada,     25,          122
+;;;    b.c,     bab,     10,           12
+;;;    ...
+;;;
+;;; We transform that into a dictionary like:
+;;; {a.c [[10N 50/122]
+;;;       [20N 25/122]
+;;;       [40N 10/122]]
+;;; ...
+;;;
+;;; Within our dictionary (ordered by entity), we have a vector
+;;; specifying the color and fraction of each author.
+
 (defn- read-csv-from
   [file]
   (with-open [in-file (io/reader file)]
@@ -23,25 +41,45 @@
         (bigint color)
         (rand-int 256)))))
 
-(defn- rows->colored-ownership
-  [rows author-colors]
-  (let [pick-color-of (color-picker-from author-colors)]
-    (for [row rows
-          :let [author (get row 1)
-                color (pick-color-of author)
-                ownership (as-ownership-fraction row)]]
-      [author [color ownership]])))
+(defn- update-author-ownership
+  [pick-color-of known-ownership row]
+  (let [author (get row 1)
+        color (pick-color-of author)
+        ownership (as-ownership-fraction row)]
+    (conj known-ownership
+          [color ownership])))
+
+(defn- update-ownership
+  [owners pick-color-of row]
+  (let [entity (get row 0)
+        updater (partial update-author-ownership pick-color-of)]
+    (update-in owners
+               [entity]
+               (fnil updater [])
+               row)))
+
+(defn- rows->entity-owners
+  ([rows author-colors]
+     (rows->entity-owners
+      rows
+      (color-picker-from author-colors)
+      {}))
+  ([rows pick-color-of owners]
+     (if (seq rows)
+       (recur
+        (rest rows)
+        pick-color-of
+        (update-ownership owners pick-color-of (first rows)))
+       owners)))
 
 (defn build-ownership-model
   [ownership-by-author author-colors]
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
   ; setup function returns initial state.
-  (->>
-   (rows->colored-ownership
-    (read-csv-from ownership-by-author)
-    (read-csv-from author-colors))
-   (into {})))
+  (rows->entity-owners
+   (read-csv-from ownership-by-author)
+   (read-csv-from author-colors)))
 
 (def ^:const fractal-size 150)
 (def ^:const figure-area (* fractal-size fractal-size))
@@ -99,9 +137,10 @@
              (drawer (ownership-of row) space-left)))))
 
 (defn draw-fractal-figures
-  [colored-ownership]
+  [entity-to-visualize entities-ownership]
   (q/background 250)
-  (let [rows (sort-by-ownership (vals colored-ownership))
+  (let [colored-ownership (entities-ownership entity-to-visualize)
+        rows (sort-by-ownership colored-ownership)
         indexed-rows (map-indexed vector rows)]
     (q/with-translation [(- (/ (q/width) 2) (/ fractal-size 2))
                          (- (/ (q/height) 2) (/ fractal-size 2))]
@@ -109,12 +148,12 @@
   (q/no-loop)) ; run once
 
 (defn visualize
-  [ownership-file color-file]
+  [ownership-file color-file entity-to-visualize]
   (q/defsketch ownership-fractals
-    :title "Knowledge Ownership visualized by Fractal Figures"
+    :title (str "Knowledge Ownership of '" entity-to-visualize "' as Fractal Figures")
     :size [500 500]
     :setup (partial build-ownership-model ownership-file color-file)
-    :draw draw-fractal-figures
+    :draw (partial draw-fractal-figures entity-to-visualize)
     ; The functional-mode middleware allows us to inject 
     ; state into our draw function:
     :middleware [m/fun-mode]))
