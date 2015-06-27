05ec03d2e8d1c99337dc155c64b5d2c6962ace59
Adam Tornhill <adam@adamtornhill.com>
2015-06-27 14:57:14 +0200

    Exclude Clojure from the Quil import

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.2.5"]
+                 [quil "2.2.5" :exclusions [org.clojure/clojure]]
                  [org.clojure/data.csv "0.1.2"]])
